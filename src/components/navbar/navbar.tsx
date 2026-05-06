ee1cb2b256f9c9ae559e484188f175663c92966f
Akanni Silva <akanni029@gmail.com>
2026-05-06 19:10:15 -0300

    fix(navbar)- corrigi estilização da navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -9,7 +9,7 @@ function Navbar() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-20">
             <div className="flex items-center">
-              <div className="flex-shrink-0 flex items-center">
+              <div className="shrink-0 flex items-center">
                 <Shield className="h-8 w-8 text-rich-cerulean" />
                 <span className="ml-2 text-2xl font-bold text-yale-blue">
                   {companyInfo.name}
