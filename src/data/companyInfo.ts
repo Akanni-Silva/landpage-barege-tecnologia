fcf741a3f727b61060fea43e3e762dffd8fd7cd2
Akanni Silva <akanni029@gmail.com>
2026-05-06 02:08:38 -0300

    fix(companyInfo)- corrigi contato da compania

--- src/data/companyInfo.ts
+++ src/data/companyInfo.ts
@@ -12,7 +12,7 @@ export const companyInfo: CompanyInfo = {
   address: "Av. dos Autonomistas, 2561 - Osasco",
   email: "baregetecnologia@gmail.com",
   instagram: "@baregetecnologia",
-  whatsappNumber: "5511998606204",
+  whatsappNumber: "(11) 9 9860-6204",
   whatsappLink:
     "https://wa.me/5511998606204?text=Ol%C3%A1!%20Gostaria%20de%20saber%20mais%20sobre%20o%20certificado%20digital.",
 };
